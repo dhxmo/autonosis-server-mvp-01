5d60605408035994bc8c34f2f43a4e9cb23f61cd
dhxmo <dhrvmohapatra@gmail.com>
2025-02-19 22:56:46 +0530

    ease up cors

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="http://192.168.0.105:3000",
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
