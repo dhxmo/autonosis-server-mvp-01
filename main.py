85557c667e3f48c3f85b46dd025eb147ae779a50
dhxmo <dhrvmohapatra@gmail.com>
2025-02-19 22:37:36 +0530

    string the whole thing together

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import uuid
 from pathlib import Path
 from typing import Dict
 
-from fastapi import FastAPI, WebSocket
+import transformers
+from fastapi import FastAPI, WebSocket, Request
 from starlette.middleware.cors import CORSMiddleware
 import aiofiles
 from starlette.websockets import WebSocketDisconnect
+
+from model import transcribe_audio_file, llm
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="http://localhost:3000",
+    allow_origins="http://192.168.0.105:3000",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,12 +23,38 @@ app.add_middleware(
 
 Path("media").mkdir(exist_ok=True)
 
+pipeline = transformers.pipeline(
+    "text-generation",
+    model="microsoft/phi-4",
+    model_kwargs={"torch_dtype": "auto"},
+    device_map="auto",
+)
+
+
 @app.get("/get_template")
-def get_template(modality:str, organ:str):
+def get_template(modality: str, organ: str):
     with open("assets/findings_template.json", "r") as f:
         json_data = json.load(f)
 
     return {"findings_template": json_data[modality.lower()][organ.lower()]}
+
+
+@app.post("/update_text")
+async def update_text(request: Request):
+    req_body = await request.json()
+
+    audio_file = f"media/{str(req_body['audio_uuid'])}.webm"
+
+    # transcribe audio file -> req_body["audio_uuid"]
+    audio_text = transcribe_audio_file(audio_file)
+
+    # call llm --> req_body["curr_text"] + audio_text
+    updated_text = llm(
+        pipeline=pipeline, prev_diagnosis=req_body["curr_text"], user_prompt=audio_text
+    )
+
+    return {"updated_text": updated_text}
+
 
 class ConnectionManager:
     def __init__(self):
@@ -41,16 +70,14 @@ class ConnectionManager:
         self.recording_files[client_id] = f"media/{audio_uuid}.webm"
 
         # Send UUID back to client immediately
-        await websocket.send_json({
-            "event_type": "audio_uuid",
-            "uuid": audio_uuid
-        })
+        await websocket.send_json({"event_type": "audio_uuid", "uuid": audio_uuid})
 
     def disconnect(self, client_id: str):
         if client_id in self.active_connections:
             self.active_connections.pop(client_id)
             if client_id in self.recording_files:
                 self.recording_files.pop(client_id)
+
 
 manager = ConnectionManager()
 
--- model.py
+++ model.py
@@ -1,112 +1,38 @@
-import json
-import os
-import shutil
-from pathlib import Path
-from typing import Any
-
-import open_clip
-import torch
-from PIL import Image
-from fastapi import FastAPI, UploadFile, File, HTTPException, Form
-from ray import serve
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import transformers
+from pywhispercpp.model import Model
 
 
-app = FastAPI()
+def transcribe_audio_file(file_path):
+    model = Model("base.en")
+    segments = model.transcribe(file_path)
 
-# Configuration
-UPLOAD_FOLDER = "uploads"
-ALLOWED_EXTENSIONS = {".png", ".jpg", ".jpeg", ".gif"}
-MAX_FILE_SIZE = 16 * 1024 * 1024  # 16MB
+    all_text = ""
+    for segment in segments:
+        all_text += segment.text + " "
 
-# Create upload directory
-Path(UPLOAD_FOLDER).mkdir(exist_ok=True)
-
-
-async def validate_file_extension(filename: str) -> bool:
-    """Validate file extension"""
-    return os.path.splitext(filename)[1].lower() in ALLOWED_EXTENSIONS
+    return all_text
 
 
-@serve.deployment(num_replicas=1, ray_actor_options={"num_cpus": 0.2, "num_gpus": 0.2})
-@serve.ingress(app)
-class CaptionXrayChest:
-    def __init__(self):
-        self.model, _, self.transform = open_clip.create_model_and_transforms(
-            model_name="coca_ViT-L-14", pretrained="model/chest_xray_findings.pt"
-        )
+def llm(pipeline, prev_diagnosis, user_prompt):
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a radiologists typing assitant. "
+            "You need to edit a new text against the previous provided one. Edit the "
+            "prev_diagnosis to accommodate what the new_diagnosis wants to add. "
+            "Maintain the structure of prev_diagnosis, just update the information from "
+            "new_diagnosis into prev_diagnosis. Provide the result in the exact same "
+            "format as the prev_diagnosis. If markdown characters \n \t are present return them as is."
+            "There might be transcription errors in new_diagnosis due to misunderstanding. "
+            "If words don't align with the context of radiology, edit them for what you see fit. "
+            "Remember: provide the result in the exact same format as the prev_diagnosis.",
+        },
+        {
+            "role": "user",
+            "content": f"prev_diagnosis: {prev_diagnosis} \n\n new_diagnosis: {user_prompt}",
+        },
+    ]
 
-    @app.post("/infer")
-    async def infer(
-        self, file: UploadFile = File(...), data: str = Form(...)
-    ) -> dict[str, str | None]:
-        extra_data = json.loads(data)  # Convert JSON string back to dictionary
-        print("extra_data", extra_data["template_findings"])
+    outputs = pipeline(messages, max_new_tokens=128)
+    response = outputs[0]["generated_text"][-1]
 
-        try:
-            contents = file.file.read()
-            with open(os.path.join(UPLOAD_FOLDER, file.filename), "wb") as f:
-                f.write(contents)
-        except Exception:
-            raise HTTPException(status_code=500, detail="Something went wrong")
-        finally:
-            file.file.close()
-
-        caption = self.generate_caption(file.filename)
-        return {"updated_findings": caption}
-
-    def generate_caption(self, img_path) -> str:
-
-        im = Image.open(img_path).convert("RGB")
-        im = self.transform(im).unsqueeze(0)
-
-        with torch.no_grad(), torch.cuda.amp.autocast():
-            generated = self.model.generate(im)
-
-        return (
-            open_clip.decode(generated[0])
-            .split("<end_of_text>")[0]
-            .replace("<start_of_text>", "")
-        )
-
-
-@serve.deployment(num_replicas=1, ray_actor_options={"num_cpus": 0.8, "num_gpus": 1})
-class MedModel:
-    def __init__(self):
-        self.pipeline = transformers.pipeline(
-            "text-generation",
-            model="microsoft/phi-4",
-            model_kwargs={"torch_dtype": "auto"},
-            device_map="auto",
-        )
-
-    @app.post("/chat")
-    async def chat(self, data: str = Form(...)) -> dict[str, Any]:
-        extra_data = json.loads(data)
-        user_prompt = extra_data["audio_data"]
-        prev_findings = extra_data["prev_findings"]
-
-        messages = [
-            {
-                "role": "system",
-                "content": "You need to edit a given finding with the template provided. Edit the "
-                "template_finding to accommodate what the new_finding wants to add. "
-                "Maintain the structure of template_finding, just update the information from "
-                "new_findings into template_findings. Provide the result in the exact same "
-                "format as the template_findings.",
-            },
-            {
-                "role": "user",
-                "content": f"template_findings: {prev_findings} \n\n new_findings: {user_prompt}",
-            },
-        ]
-
-        outputs = self.pipeline(messages, max_new_tokens=128)
-        response = outputs[0]["generated_text"][-1]
-
-        return {"updated_audio_text": response}
-
-
-# caption_app = CaptionXrayChest.bind()
-caption_app = MedModel.bind()
+    return response
